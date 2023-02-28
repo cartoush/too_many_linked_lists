ea23af5332d7569ff1565b7925f52fac01cbd241
cartoush <thibautartis@pm.me>
2023-03-01 00:18:55 +0100

    third ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-mod first;
-mod second;
+pub mod first;
+pub mod second;
+pub mod third;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
--- src/second.rs
+++ src/second.rs
@@ -208,6 +208,11 @@ mod test {
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 
     #[test]
@@ -216,6 +221,11 @@ mod test {
         list.push(1);
         list.push(2);
         list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
 
         let mut iter = list.iter_mut();
         assert_eq!(iter.next(), Some(&mut 3));
